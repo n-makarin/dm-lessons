a334f1becd53504b6a3fa816b0a7e4c4c70a0804
Nikita Makarin <zn.makarin@gmail.com>
2020-06-18 21:17:06 +0300

    Add overlay listeners

--- webpack/src/lessons/3/index.js
+++ webpack/src/lessons/3/index.js
@@ -1,9 +1,13 @@
 let cart = null;
 let cartNumber = 0;
+let orderList = null;
+let orderListOverlay = null;
 
 function init() {
   const productCurdButton = document.querySelector('.product-card__add-to-cart');
   cart = document.querySelector('.cart');
+  orderList = document.querySelector('.order-list');
+  orderListOverlay = document.querySelector('.order-list__overlay');
 
   function incrementCardNumber() {
     cartNumber += 1;
@@ -19,7 +23,17 @@ function init() {
     setCartNumber();
   }
 
+  function showOrderListModal() {
+    orderList.classList.add('order-list_visible');
+  }
+
+  function hideOrderListModal() {
+    orderList.classList.remove('order-list_visible');
+  }
+
   productCurdButton.addEventListener('click', addProductToCart);
+  cart.addEventListener('click', showOrderListModal);
+  orderListOverlay.addEventListener('click', hideOrderListModal);
 }
 
 export default init;
