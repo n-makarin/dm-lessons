540a262bbb630de78d0fb8762a5a69c0b915dc55
Nikita Makarin <zn.makarin@gmail.com>
2020-06-18 20:47:04 +0300

    Add cart methods to lesson-3/index.js

--- webpack/src/lessons/3/index.js
+++ webpack/src/lessons/3/index.js
@@ -1,3 +1,25 @@
-export default () => {
-  console.log('hello');
-};
+let cart = null;
+let cartNumber = 0;
+
+function init() {
+  const productCurdButton = document.querySelector('.product-card__add-to-cart');
+  cart = document.querySelector('.cart');
+
+  function incrementCardNumber() {
+    cartNumber += 1;
+  }
+
+  function setCartNumber() {
+    cart.innerText = cartNumber;
+  }
+
+  function addProductToCart() {
+    cartNumber = Number(cart.innerText);
+    incrementCardNumber();
+    setCartNumber();
+  }
+
+  productCurdButton.addEventListener('click', addProductToCart);
+}
+
+export default init;
